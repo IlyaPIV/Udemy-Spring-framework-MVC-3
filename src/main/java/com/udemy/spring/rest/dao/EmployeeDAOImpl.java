796523859da09549fc05e1fdcba87fd2538b185a
p_Ilya <petermoloch@mail.ru>
2022-08-10 20:16:00 +0300

    REST @GET for list and single Employee

---
+++ src/main/java/com/udemy/spring/rest/dao/EmployeeDAOImpl.java
@@ -0,0 +1,51 @@
+package com.udemy.spring.rest.dao;
+
+import com.udemy.spring.rest.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("from Employee", Employee.class).getResultList();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Employee.class, id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+//        Employee employee = session.get(Employee.class, id);
+//        session.delete(employee);
+
+        Query<Employee> query = session.createQuery("delete from Employee where id = :employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
+}
